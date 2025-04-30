5e8c120114a06c5928d14314bcddb59b9220c966
halilserkanuz <halilserkanuz@gmail.com>
2025-04-30 14:58:47 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-[32px] sm:text-[48px] font-bold text-center">
+          Welcome to the Jobstbd Template App
+        </h1>
+        <p className="text-[16px] sm:text-[20px] text-center">
+          This is a simple example of a Next.js application.
+        </p>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        
+      </footer>
+    </div>
+  );
+}
